3924388ade4d4481634dd459275bc3a94d1a3729
Jomin John <jominjhn@googlemail.com>
2025-09-20 03:43:44 +0100

    Implementation of 3 of item 'A' special offer.

--- CheckoutKata.Core/Checkout.cs
+++ CheckoutKata.Core/Checkout.cs
@@ -2,30 +2,49 @@ namespace CheckoutKata.Core;
 
 public class Checkout
 {
-    private int _totalPrice = 0;
+    private int _countA = 0;
+    private int _countB = 0;
+    private int _countC = 0;
+    private int _countD = 0;
 
     public void Scan(string item)
     {
         if (item == "A")
         {
-            _totalPrice += 50;
+            _countA++;
         }
         else if (item == "B")
         {
-            _totalPrice += 30;
+            _countB++;
         }
         else if (item == "C")
         {
-            _totalPrice += 20;
+            _countC++;
         }
         else if (item == "D")
         {
-            _totalPrice += 15;
+            _countD++;
         }
     }
 
     public int GetTotalPrice()
     {
-        return _totalPrice;
+        int total = 0;
+
+        // Item A: 50 each, 3 for 130
+        int specialOfferA = _countA / 3;
+        int remainingA = _countA % 3;
+        total += specialOfferA * 130 + remainingA * 50;
+
+        // Item B: 30 each
+        total += _countB * 30;
+
+        // Item C: 20 each
+        total += _countC * 20;
+
+        // Item D: 15 each
+        total += _countD * 15;
+
+        return total;
     }
 }
